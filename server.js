a1eea900a5abb08527c20b03d9816134683453b2
thuan.nv <nguyenthuankma@gmail.com>
2023-03-11 15:07:41 +0700

    update deploy images

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+server.use(express.static(__dirname + "/public"));
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
