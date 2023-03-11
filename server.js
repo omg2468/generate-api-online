e2cfc5b7df0d0f713cf9428d41cf0799affc8e81
thuan.nv <nguyenthuankma@gmail.com>
2023-03-11 16:01:03 +0700

    show api hompages

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
+
+// Serve homepage with links to all APIs
+server.get('/', (req, res) => {
+    const resources = Object.keys(router.db.__wrapped__)
+    const links = resources.map(resource => `<li><a href="/${resource}">${resource}</a></li>`)
+    res.send(`<h1>APIs:</h1><ul>${links.join('')}</ul>`)
+})
+
 server.use(router)
 server.listen(process.env.PORT || 3000, () => {
     console.log('JSON Server is running')
